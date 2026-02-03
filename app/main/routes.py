5dd00bc541d3921f3dff437eb7542075ecaa8886
Ornilio-Neto <portifolio084@gmail.com>
2026-02-03 13:46:50 -0300

    correcao2

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request, session, jsonify, abort
+from flask import render_template, flash, redirect, url_for, request, session, jsonify, abort, current_app
 from flask_login import login_user, logout_user, current_user, login_required
 from . import bp
 from app import db, oauth
